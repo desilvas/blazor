a2bceeb3859c1555493937dd2f3fef5b9a06d79d
shalinds <shalinds@gmail.com>
2019-06-22 15:08:46 +0530

    files added

---
+++ ToDoApp/Data/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Data
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string ItemText { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
---
+++ ToDoApp/Data/ToDoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Data
+{
+    public class ToDoService
+    {
+        public Task<List<ToDo>> GetToDos()
+        {
+            var list = new ToDo[]
+            {
+                new ToDo
+                {
+                    Id = 1,
+                    Completed = true,
+                    ItemText = "This is the first item"
+                },
+
+               new ToDo
+                {
+                    Id = 2,
+                    Completed = false,
+                    ItemText = "This is the second item"
+                }
+            };
+
+            return Task.FromResult(list.ToList());
+
+        }
+    }
+}
